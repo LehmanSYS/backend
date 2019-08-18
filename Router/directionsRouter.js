77e804e02fd230480f4c9a42d4ec9dc9c18786b2
Sami <BlueBubbleBeam@outlook.com>
2019-08-18 18:41:49 -0400

    created direction rputer

--- Router/directionsRouter.js
+++ Router/directionsRouter.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const router = express.Router();
-import axios from 'axios';
+const axios = require('axios');
+const api = require('../Api');
 
-router.get('/', (req,res) =>{
-    let url = 'https://maps.googleapis.com/maps/api/directions/json?'+
-    'origin=40.768176,-73.96485&'+ //USERS LATITUDE, LONGITUDE HERE
-    'destination=' + this.state.lat + ',' + this.state.lon + '&'+
-    '&mode=transit&key=' + process.env.API_KEY;
-    await axios.get(url)
-    .then(r => {
-        res.status(200).send(r);
+router.get('/', (req,res,next) =>{ //returns the route to destionation for one user
+    await axios.get(req)            //must contain a response body so api works
+    .then(data => {
+        res.status(200).send(data);
     })
-    .catch(e => {
-        res.status(404).send(e);
+    .catch(err => {
+        res.status(404).send(err);
     })
 })
