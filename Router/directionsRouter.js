743decaf453e5475964eb51e757b634ac76a9887
Kyrylo Glamazdin <kyrylo.glamazdin36@myhunter.cuny.edu>
2019-08-21 16:21:50 -0400

    made router changes

--- Router/directionsRouter.js
+++ Router/directionsRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const api = require('../Api');
+const {getUserRoute, getUserETA} = require('../Api');
 
 router.post('/', async (req,res,next) =>{ //returns the route to destionation for one user
     let userPaths = [];
@@ -11,7 +11,21 @@ router.post('/', async (req,res,next) =>{ //returns the route to destionation fo
             latitude: req.body.newGroup.latitude,
             longitude: req.body.newGroup.longitude
         }
-        userPaths.push(await api(request));
+        userPaths.push(await getUserRoute(request));
+    }
+    res.status(200).send(userPaths);                 //must contain a response body so api works
+})
+
+router.post('/eta', async (req,res,next) =>{ //returns the route to destionation for one user
+    let userPaths = [];
+    console.log(req.body);
+    for (let i = 0; i < req.body.workingGroup.users.length; i++){
+        let request = {
+            user: req.body.workingGroup.users[i],
+            latitude: req.body.workingGroup.latitude,
+            longitude: req.body.workingGroup.longitude
+        }
+        userPaths.push(await getUserETA(request));
     }
     res.status(200).send(userPaths);                 //must contain a response body so api works
 })
