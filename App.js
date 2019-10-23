c673a0b46b4fe40a9aa14f4fd6cb316768e62c11
Sami <BlueBubbleBeam@outlook.com>
2019-10-23 17:13:54 -0400

    add port method

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ module.exports = groupHandler;
 require('./Socket')(io);
 
 const apiRouter = require("./Router/apiRouter");
+const router = express.Router();
 const bodyParser = require("body-parser");
 const {db} = require("./Database");
 const seed = require('./Data/Seed');
@@ -36,4 +37,9 @@ db.sync({ force: false }).then(async () => {
   app.use(cors()); // <---- use cors middleware
 
   app.use("/api", apiRouter);
+
+  router.get('/', (req,res) =>{
+    res.status(200).send(PORT);
+  })
+  app.use("/port", router);
 });
